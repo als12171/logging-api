25fa18753554d95881a79aea87683b107d31cfa0
als12171 <als12171@github.com>
2021-03-15 23:34:03 +0000

    logger

--- api/logger.js
+++ api/logger.js
@@ -1,5 +1,5 @@
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 function logDebug(req, res) {
     try {
--- index.js
+++ index.js
@@ -6,18 +6,13 @@ var app = express();
 // Set server port
 app.set('port', (process.env.PORT || 5000));
 
-// Set express static folder
-app.use(express.static(__dirname + '/public'));
-
 // Configure base route
-app.get('/', function(request, response) {
-  response.render('index');
-});
+app.get('/', function (request, response) {});
 
-// Load APIs in separate files 
+// Load APIs in separate files
 require('./api/logger.js')(app);
 
 // Start the application!
-app.listen(app.get('port'), function() {
-  log.info('Node app is running on port', app.get('port'));
+app.listen(app.get('port'), function () {
+    log.info('Node app is running on port', app.get('port'));
 });
